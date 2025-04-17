264416c5454484ab68e4ea33c297095930791588
VicVaporub14 <212310861@itslerdo.edu.mx>
2025-04-17 15:48:59 -0600

    Agregacion de Funcion getImagePath para mostrar imagen segun en la base de datos

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { formatCurrency } from "@/src/utils"
+import { formatCurrency, getImagePath } from "@/src/utils"
 import { Product } from "@prisma/client"
 import Image from "next/image"
 import AddProductButton from "./AddProductButton"
@@ -8,22 +8,23 @@ type ProductCardProps ={
 }
 
 export default function ProductCard({product}: ProductCardProps) {
-  return (
-    <div className="border border-gray-200 bg-white rounded-lg overflow-hidden shadow-lg">
-        <Image
-            width={400}
-            height={500}
-            src={`/products/${product.image}.jpg`}
-            alt={`Imagen producto ${product.name}`}
-        />
-
-        <div className="p-5">
-            <h3 className="font-bold text-lg">{product.name}</h3>
-            <p className="mt-2 font-black text-xl text-amber-400">{formatCurrency(product.price)}</p>
-            <AddProductButton 
-                product={product}
+    const imagePath = getImagePath(product.image)
+    return (
+        <div className="border border-gray-200 bg-white rounded-lg overflow-hidden shadow-lg">
+            <Image
+                width={400}
+                height={500}
+                src={imagePath}
+                alt={`Imagen producto ${product.name}`}
             />
+
+            <div className="p-5">
+                <h3 className="font-bold text-lg">{product.name}</h3>
+                <p className="mt-2 font-black text-xl text-amber-400">{formatCurrency(product.price)}</p>
+                <AddProductButton 
+                    product={product}
+                />
+            </div>
         </div>
-    </div>
-  )
+    )
 }
